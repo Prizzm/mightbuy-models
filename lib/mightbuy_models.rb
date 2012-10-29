af6f145e1d4f472dfbdde70dcce38b0a78d8848b
Hemant Kumar <gethemant@gmail.com>
2012-10-29 17:37:17 +0530

    Add config directory and fix specs

--- lib/mightbuy_models.rb
+++ lib/mightbuy_models.rb
@@ -1,4 +1,7 @@
 $:<< File.expand_path("mightbuy_models", __FILE__)
 
+require "active_record"
+require "devise"
 require "mightbuy_models/version"
 require "mightbuy_models/railtie"
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,30 @@
 __LIB_DIR__ = File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
+current_dir = File.expand_path(File.dirname(__FILE__))
 # Configure Rails Environment
 
 $LOAD_PATH.unshift __LIB_DIR__
 
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
+require "bundler"
+Bundler.require(:default,'test')
+
+require_relative "#{current_dir}/../config/devise_initializer"
 require "mightbuy_models"
 
 Rails.backtrace_cleaner.remove_silencers!
-db_connection()
 
-Dir["spec/support/*.rb"].each {|file| require file }
-Dir["app/models/*.rb"].each { |model| require model }
+require "yaml"
+begin
+  db_config = YAML.load_file(File.join(File.dirname(__FILE__), "database.yml"))['test']
+  db_config.symbolize_keys!
+  ActiveRecord::Base.establish_connection(db_config)
+rescue
+  puts $!.message
+  puts "Error connecting with credentials #{db_config.inspect}"
+end
+
+Dir["#{current_dir}/spec/support/*.rb"].each {|file| require file }
+Dir["#{current_dir}/../app/models/**/*.rb"].each { |model| require model }
 
 RSpec.configure do |config|
   config.mock_with :rspec
@@ -29,14 +44,3 @@ RSpec.configure do |config|
   end
 end
 
-def db_connection
-  require "yaml"
-  begin
-    db_config = YAML.load_file(File.join(File.dirname(__FILE__), "database.yml"))['test']
-    db_config.symbolize_keys!
-    ActiveRecord::Base.establish_connection(db_config)
-  rescue
-    puts $!.message
-    puts "Error connecting with credentials #{db_config.inspect}"
-  end
-end
