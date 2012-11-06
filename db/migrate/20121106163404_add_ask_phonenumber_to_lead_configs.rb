3a16d2f723588e6f9a152a84432f311f25498088
Hemant Kumar <gethemant@gmail.com>
2012-11-06 23:50:40 +0530

    Revert "Add phone number"
    This reverts commit 02266f03a828b46c3fed1c2a80bd5f3299a407c4.

--- db/migrate/20121106163404_add_ask_phonenumber_to_lead_configs.rb
+++
@@ -1,9 +0,0 @@
-class AddAskPhonenumberToLeadConfigs < ActiveRecord::Migration
-  def up
-    add_column :lead_configs, :ask_for_phonenumber, :boolean
-  end
-  
-  def down
-     remove_column :lead_configs, :ask_for_phonenumber
-   end
-end
