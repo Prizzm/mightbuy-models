2440aa68840b8232bd0d30f3b4b45b947c3065d5
Iszak Bryan <iszak@karmarama.com>
2012-12-02 01:39:42 +0000

    Fixed error

--- db/migrate/20121105140520_populate_default_lead_configs.rb
+++ db/migrate/20121105140520_populate_default_lead_configs.rb
@@ -1,6 +1,6 @@
 class PopulateDefaultLeadConfigs < ActiveRecord::Migration
   def up
-    LeadConfig.reset_column_information
+    BusinessConfig.reset_column_information
     Business.all.each do |business|
       business.create_lead_config! unless business.lead_config
     end
