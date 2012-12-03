4c4ba0a40727849b7ba2b675cf942efb1f5db117
Iszak Bryan <iszak@karmarama.com>
2012-12-03 21:33:33 +0000

    Added bargin conditions

--- app/models/bargin.rb
+++ app/models/bargin.rb
@@ -1,6 +1,25 @@
 class Bargin < ActiveRecord::Base
   belongs_to :product
   has_many :anti_forge_tokens
+  has_many :bargin_conditions
+
+  attr_accessible :name,
+                  :offer,
+                  :type,
+                  :description,
+                  :url,
+                  :value,
+                  :bargin_type,
+                  :barcode,
+                  :type,
+                  :accept_payments,
+                  :payment_type,
+                  :discount
+
+  attr_accessible :bargin_conditions_attributes
 
   validates :product, presence: true
+
+
+  accepts_nested_attributes_for :bargin_conditions
 end
